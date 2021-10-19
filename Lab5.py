2ccbc60cafc6d8ab46cf2264fd7f4abdcf81c65b
alexhaurin <92339236+alexhaurin@users.noreply.github.com>
2021-10-19 13:45:02 -0400

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,15 +1,32 @@
-namePrompt = "What's your name?: "
+from random import randrange
+
 name = str("")
+guessCount = int(0)
 
 # Get name
 while (True):
     try:
-        name = str(input(namePrompt))
+        name = str(input("What's your name?: "))
         print("Hello, " + name + "!")
         break
     except:
         print("Enter a valid input")
 
 # Get other input
+num = int(randrange(100))
+print("I am thinking of a number between one and 100")
 
-# Do stuff
+while (True):
+    try:
+        guess = int(input("What is your guess?: "))
+        guessCount += 1
+        
+        if (guess == num):
+            print("Nice! You guessed it in " + str(guessCount) + " tries!")
+            break
+        elif (guess > num):
+            print("Lower")
+        elif (guess < num):
+            print("Higher")
+    except:
+        print("Please enter an integer")
